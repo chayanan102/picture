fb9a7a1a33712acf884b0c30a2ea64228af7380d
chayanan102 <68525337+chayanan102@users.noreply.github.com>
2020-08-21 22:30:20 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,112 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">      
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class></li>
+          <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="2" class></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item">
+            <img :src="img[0].src" class="img-fluid" />
+          </div>
+          <div class="carousel-item active">
+            <img :src="img[1].src" class="img-fluid" />
+          </div>
+          <div class="carousel-item">
+            <img :src="img[2].src" class="img-fluid" />
+          </div>
+          <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <hr class="featurette-divider" />
+      <div class="row featurette alert alert-success" style="opacity: 0.9; ">
+        <div class="row align-items-center">
+          <div class="col-6">
+            <!-- 2.รับค่าจาก emit @cc แล้วส่งไปทำงานใน function "sum"-->
+            <emotion label @cc="sum" />
+          </div>
+          <div class="col-6">
+            <img :src="img[0].src" class="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <hr class="featurette-divider" />
+      <div class="row featurette alert alert-success" style="opacity: 0.9; ">
+        <div class="row align-items-center">
+          <div class="col-6">
+            <img :src="img[1].src" class="img-fluid" />
+          </div>
+          <div class="col-6">
+            <!-- 2.รับค่าจาก emit @cc แล้วส่งไปทำงานใน function "sum"-->
+            <emotion label @cc="sum" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <hr class="featurette-divider" />
+      <div class="row featurette alert alert-success" style="opacity: 0.9; ">
+        <div class="row align-items-center">
+          <div class="col-6">
+            <!-- 2.รับค่าจาก emit @cc แล้วส่งไปทำงานใน function "sum"-->
+            <emotion label @cc="sum" />
+          </div>
+          <div class="col-6">
+            <img :src="img[2].src" class="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import emotion from "./components/emotion.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { emotion },
+  data: function () {
+    return {
+      img: [
+        {
+          src:
+            "https://discordapp.com/channels/746380296630567082/746380296630567085/746380470807167027",
+        },
+        {
+          src:
+            "https://discordapp.com/channels/746380296630567082/746380296630567085/746380478365565130",
+        },
+        {
+          src:
+            "https://discordapp.com/channels/746380296630567082/746380296630567085/746380485969575947",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Do Hyeon", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
   margin-top: 60px;
 }
+
 </style>
---
+++ src/components/emotion.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="example-1">
+    <!--<button @click="upFunction()" class="btn btn-info">{{label}}</button>-->
+    <p v-bind:style="{fontSize: fsize + 'px' }"> Score : {{ counter }} </p>
+    <div class="row-md-10 "> 
+    <button @click="LikeFunction()" class="btn btn-info">
+      <i class="fas fa-thumbs-up" style='color:white'></i>
+      <img src="./images/like.png" class="img-fluid" />
+    </button>
+    <button @click="LoveFunction()" class="btn btn-danger">
+      <i class="fas fa-heart"></i>
+      </button>
+    <button @click="HahaFunction()" class="btn btn-warning">
+      <i class="fas fa-laugh-squint" style='color:white'></i>
+    </button>
+    <button @click="WowFunction()" class="btn btn-success">
+      <i class="fas fa-surprise"></i>
+      </button>
+    <button @click="SadFunction()" class="btn btn-info">
+      <i class="far fa-frown-open"></i>
+      </button>
+    <button @click="AngryFunction()" class="btn btn-danger">
+      <i class="fas fa-angry"></i>
+      </button>
+    </div>
+      
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+    }
+  },
+  methods:{
+    LikeFunction: function(){
+      this.counter += 1
+      this.$emit('cc',this.counter)
+    },
+    LoveFunction: function(){
+      this.counter += 10
+      this.$emit("cc", this.counter);
+    },
+    HahaFunction: function(){
+      this.counter += 5
+      this.$emit("cc", this.counter);
+    },
+    WowFunction: function(){
+      this.counter += 20
+      this.$emit("cc", this.counter);
+    },
+    SadFunction: function(){
+      this.counter -= 5
+      this.$emit("cc", this.counter);
+    },
+    AngryFunction: function(){
+      this.counter -= 50
+      this.$emit("cc", this.counter);
+    },
+    ResetFunction: function () {
+      this.counter = 0;
+      this.$emit("cc", this.counter);
+    },
+    sum(counterValue){
+      this.total += counterValue
+    },
+  },
+  props: {
+    label: String
+  },
+};
+</script>
+
+<style>
+</style>
