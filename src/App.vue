2c44b143163ef350734e511607dfe58502051e43
chayanan102 <68525337+chayanan102@users.noreply.github.com>
2020-08-21 23:31:03 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
     <div class="container">      
       <div id="myCarousel" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class></li>
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
           <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="2" class></li>
+          <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
         </ol>
         <div class="carousel-inner">
           <div class="carousel-item">
@@ -30,10 +30,9 @@
     </div>
     <div class="container">
       <hr class="featurette-divider" />
-      <div class="row featurette alert alert-success" style="opacity: 0.9; ">
+      <div class="row featurette alert alert-success" style="opacity: 1.0; ">
         <div class="row align-items-center">
           <div class="col-6">
-            <!-- 2.รับค่าจาก emit @cc แล้วส่งไปทำงานใน function "sum"-->
             <emotion label @cc="sum" />
           </div>
           <div class="col-6">
@@ -50,7 +49,6 @@
             <img :src="img[1].src" class="img-fluid" />
           </div>
           <div class="col-6">
-            <!-- 2.รับค่าจาก emit @cc แล้วส่งไปทำงานใน function "sum"-->
             <emotion label @cc="sum" />
           </div>
         </div>
@@ -82,17 +80,11 @@ export default {
     return {
       img: [
         {
-          src:
-            "https://discordapp.com/channels/746380296630567082/746380296630567085/746380470807167027",
-        },
+          src: "https://discordapp.com/channels/746380296630567082/746380296630567085/746380470807167027", },
         {
-          src:
-            "https://discordapp.com/channels/746380296630567082/746380296630567085/746380478365565130",
-        },
+          src: "https://discordapp.com/channels/746380296630567082/746380296630567085/746380478365565130", },
         {
-          src:
-            "https://discordapp.com/channels/746380296630567082/746380296630567085/746380485969575947",
-        },
+          src: "https://discordapp.com/channels/746380296630567082/746380296630567085/746380485969575947", },
       ],
     };
   },
@@ -101,12 +93,12 @@ export default {
 
 <style>
 #app {
-  font-family: "Do Hyeon", sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #ffffff;
-  margin-top: 60px;
+  color:#2c3e50;
+  margin-top: 100px;
 }
 
 </style>
--- src/components/emotion.vue
+++ src/components/emotion.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="example-1">
-    <!--<button @click="upFunction()" class="btn btn-info">{{label}}</button>-->
-    <p v-bind:style="{fontSize: fsize + 'px' }"> Score : {{ counter }} </p>
-    <div class="row-md-10 "> 
-    <button @click="LikeFunction()" class="btn btn-info">
-      <i class="fas fa-thumbs-up" style='color:white'></i>
-      <img src="./images/like.png" class="img-fluid" />
+  <div id="example-1">    
+    <p v-bind:style="{fontSize: fsize + 'px' }"> Score : {{ count }} </p>    
+    <button @click="LikeFunction()">
+      <!-- <i class="fas fa-thumbs-up" style='color:white'></i> -->
+      <img src="./images/like.png">
     </button>
-    <button @click="LoveFunction()" class="btn btn-danger">
-      <i class="fas fa-heart"></i>
+    <button @click="LoveFunction()">
+      <!-- <i class="fas fa-heart"></i> -->
+      <img src="./images/love.png">
       </button>
-    <button @click="HahaFunction()" class="btn btn-warning">
-      <i class="fas fa-laugh-squint" style='color:white'></i>
+    <button @click="HahaFunction()">
+      <!-- <i class="fas fa-laugh-squint" style='color:white'></i> -->
+      <img src="./images/haha.png">
     </button>
-    <button @click="WowFunction()" class="btn btn-success">
-      <i class="fas fa-surprise"></i>
+    <button @click="WowFunction()">
+      <!-- <i class="fas fa-surprise"></i> -->
+      <img src="./images/wow.png">
       </button>
-    <button @click="SadFunction()" class="btn btn-info">
-      <i class="far fa-frown-open"></i>
+    <button @click="SadFunction()">
+      <!-- <i class="far fa-frown-open"></i> -->
+      <img src="./images/sad.png">
       </button>
-    <button @click="AngryFunction()" class="btn btn-danger">
-      <i class="fas fa-angry"></i>
+    <button @click="AngryFunction()">
+      <!-- <i class="fas fa-angry"></i> -->
+      <img src="./images/angry.png">
       </button>
-    </div>
+    
       
 
   </div>
@@ -32,37 +35,37 @@
 export default {
   data: function () {
     return {
-      counter: 0,
+      count: 0,
     }
   },
   methods:{
     LikeFunction: function(){
-      this.counter += 1
-      this.$emit('cc',this.counter)
+      this.count += 1
+      this.$emit('cc',this.count)
     },
     LoveFunction: function(){
-      this.counter += 10
-      this.$emit("cc", this.counter);
+      this.count += 10
+      this.$emit("cc", this.count);
     },
     HahaFunction: function(){
-      this.counter += 5
-      this.$emit("cc", this.counter);
+      this.count += 5
+      this.$emit("cc", this.count);
     },
     WowFunction: function(){
-      this.counter += 20
-      this.$emit("cc", this.counter);
+      this.count += 20
+      this.$emit("cc", this.count);
     },
     SadFunction: function(){
-      this.counter -= 5
-      this.$emit("cc", this.counter);
+      this.count -= 5
+      this.$emit("cc", this.count);
     },
     AngryFunction: function(){
-      this.counter -= 50
-      this.$emit("cc", this.counter);
+      this.count -= 50
+      this.$emit("cc", this.count);
     },
     ResetFunction: function () {
-      this.counter = 0;
-      this.$emit("cc", this.counter);
+      this.count = 0;
+      this.$emit("cc", this.count);
     },
     sum(counterValue){
       this.total += counterValue
